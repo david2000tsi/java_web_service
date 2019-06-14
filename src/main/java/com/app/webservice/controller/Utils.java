c3ebfa2a6dba2e5ad2d9fe01da70d795b3ba1c9c
David Alves <david2000tsi@gmail.com>
2019-06-14 16:52:05 -0300

    Created resources to datetime and math methods

--- src/main/java/com/app/webservice/controller/Utils.java
+++ src/main/java/com/app/webservice/controller/Utils.java
@@ -3,11 +3,10 @@ package com.app.webservice.controller;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Utils {
 	
 	public static boolean isValidNumber(String number) {
@@ -20,42 +19,33 @@ public class Utils {
 		return true;
 	}
 	
-	public static String getTimestampStr() {
+	public static String getTimestamp() {
 		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 		return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(timestamp.getTime());
 	}
-
-	@RequestMapping(value="/gettimestamp")
-	public String getTimestamp() {
-		return getTimestampStr();
-	}
 	
-	@RequestMapping(value="/makesum/{a}/{b}")
-	public String makeSum(@PathVariable("a") String a, @PathVariable("b") String b) {
+	public static String makeSum(@PathVariable("a") String a, @PathVariable("b") String b) {
 		if(!isValidNumber(a) || !isValidNumber(b)) {
 			return "Invalid parameters!";
 		}
 		return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
 	}
 	
-	@RequestMapping(value="/makesub/{a}/{b}")
-	public String makeSub(@PathVariable("a") String a, @PathVariable("b") String b) {
+	public static String makeSub(@PathVariable("a") String a, @PathVariable("b") String b) {
 		if(!isValidNumber(a) || !isValidNumber(b)) {
 			return "Invalid parameters!";
 		}
 		return Integer.toString(Integer.parseInt(a) - Integer.parseInt(b));
 	}
 	
-	@RequestMapping(value="/makemul/{a}/{b}")
-	public String makeMul(@PathVariable("a") String a, @PathVariable("b") String b) {
+	public static String makeMul(@PathVariable("a") String a, @PathVariable("b") String b) {
 		if(!isValidNumber(a) || !isValidNumber(b)) {
 			return "Invalid parameters!";
 		}
 		return Integer.toString(Integer.parseInt(a) * Integer.parseInt(b));
 	}
 	
-	@RequestMapping(value="/makediv/{a}/{b}")
-	public String makeDiv(@PathVariable("a") String a, @PathVariable("b") String b) {
+	public static String makeDiv(@PathVariable("a") String a, @PathVariable("b") String b) {
 		if(!isValidNumber(a) || !isValidNumber(b)) {
 			return "Invalid parameters!";
 		}
